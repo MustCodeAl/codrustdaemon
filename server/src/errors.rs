ad2c271353c1b2f0c7421c5a48fb3d11d6b2959c
MustBeAL <87888006+AlNotAlbert@users.noreply.github.com>
2023-08-15 18:45:48 -0500

    server crate

--- server/src/errors.rs
+++ server/src/errors.rs
@@ -1,8 +1,12 @@
+#[derive(Debug)]
 enum CLiError {
     GenericError = 1,
     NotImplementedError = 2,
     BinaryNotFound = 3,
 }
+
+
+#[derive(Debug)]
 struct ErrorResponse  {
     code: CLiError,
     desc: String,
