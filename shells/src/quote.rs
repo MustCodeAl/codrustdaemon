165dbe614b4dd821c6f46d8c6f54627168dc5568
MustBeAL <87888006+AlNotAlbert@users.noreply.github.com>
2023-08-30 23:49:14 -0500

    server crate
    working on backend

---
+++ shells/src/quote.rs
@@ -0,0 +1,23 @@
+
+
+// todo make this use env args for the program instead
+fn quote(args: &Vec<String>) -> String {
+
+    let mut quoted = Vec::<String>::new();
+
+    for arg in args {
+        // if char is one of these, then we need to quote it
+        match arg.chars().rev().next_back().expect("not valid char") {
+             '\''| '|'| '&'| ';'| '<'| '>'| '('| ')'|
+            '$'| '`'| '\\'| '"'| ' '| '\t'| '\n'|
+            '*'| '?'| '['| ']'| '#'| '~'| '='| '%' => {
+                quoted.push(format!("\"{arg}\"" ));
+
+            }
+            _ => {
+                continue
+            }
+        }
+    }
+    quoted.join(" ")
+}
